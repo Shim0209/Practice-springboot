a391a2deb24da961717e69fe9b849a9e2c4cddfd
Shim0209 <81503611+Shim0209@users.noreply.github.com>
2021-05-28 22:57:23 +0900

    Update SecurityConfig.java

--- practice-security/src/main/java/practicesecurity/demo/config/SecurityConfig.java
+++ practice-security/src/main/java/practicesecurity/demo/config/SecurityConfig.java
@@ -13,7 +13,12 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableWebSecurity // 필터 체인에 등록 (스프링 시큐리티 활성화)
-@EnableGlobalMethodSecurity(prePostEnabled = true) // 특정 주소 접근시 권한 및 인증을 미리 체크
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+// @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")을 메서드에 걸어서 (함수시작전 수행) 여러 권한을 간편하게 그리고 한번에 설정 할 수 있다.
+// 특정 주소 접근시 권한 및 인증을 메서드 수행전 체크
+// @securedEnabled = true 요즘에는 이걸 많이 사용한다??
+// 위와 같은건 간단하게 메서드 하나에만 설정할 때 사용하는 것.
+// 글로벌 설정은 SecurityConfig를 통해서 설정할 것.
 public class SecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
 
     @Autowired
